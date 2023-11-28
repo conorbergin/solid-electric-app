d09a2df44758de2de54b798f052ca3f1c4e441a5
Conor Bergin <1299651405@mail.com>
2023-11-28 14:21:14 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  base: '/solid-electric-app/'
+  base: '/solid-electric-app/',
   plugins: [solid()],
   assetsInclude: [`**/*.wasm`],
 
